d0d90d22713b10a06192a42354654a8f6ec66b7f
Elisa Marie <rieser.elisa@johak.at>
2024-11-15 11:21:18 +0100

    update daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button class="btn">DaisyUI</button>
+<button class="btn" href="https://daisyui.com/" target="_blank">DaisyUI</button>
