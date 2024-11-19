17d93997676985990d453664032023eac34155c4
lepfeiffenberger <90902624+lepfeiffenberger@users.noreply.github.com>
2024-11-19 12:18:54 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<img src="$/static/images/bg-pattern.svg" alt="" />
+<h1 class=""><b>Simple, traffic-based pricing</b></h1>
+<p class="uue">Sign-up for our 30-day trial. No credit card required.</p>
+
+<div class="box-border h-80 w-20 border-4 p-4"></div>
