2e8d952104689ef18697bd1cdaa3718682e390f5
lepfeiffenberger <90902624+lepfeiffenberger@users.noreply.github.com>
2024-11-19 11:43:17 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,1 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<button class="btn" href="https://daisyui.com/" target="_blank">DaisyUI</button>
-
-<h1>Hallo Elisa Schaffi</h1>
+<img src="$/static/images/bg-pattern.svg" alt="" />
