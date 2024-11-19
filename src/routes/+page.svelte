d113eb93749f5a4d93cd40002b53111fdf450ec1
vguster <guster.victoria@johak.at>
2024-11-19 11:07:10 +0100

    text schaffi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,5 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <button class="btn" href="https://daisyui.com/" target="_blank">DaisyUI</button>
+
+<h1>Hallo Elisa Schaffi</h1>
