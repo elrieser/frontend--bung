4829340a2984b761cc86edd1dd798fe943dd9126
Elisa Marie <rieser.elisa@johak.at>
2024-11-22 09:42:12 +0100

    change box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1 class=""><b>Simple, traffic-based pricing</b></h1>
 <p class="uue">Sign-up for our 30-day trial. No credit card required.</p>
 
-<div class="box-border h-80 w-20 border-4 p-4"></div>
+<div class="w-30 box-border h-80 border-4 p-4"></div>
