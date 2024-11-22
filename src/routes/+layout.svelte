5febb1751bb61d3b67fd55368dfb05ccd0bcc97d
lepfeiffenberger <90902624+lepfeiffenberger@users.noreply.github.com>
2024-11-22 11:18:57 +0100

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,25 @@
 	let { children } = $props();
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-evenly bg-blue-100">
+<div
+	class="background-color: rgba( flex min-h-screen flex-col items-center justify-evenly bg-blue-100"
+>
 	{@render children()}
+</div>
+
+<div>
+	<div class="flex justify-end">
+		<div class="flex gap-1">
+			<p>Monthly Billing</p>
+
+			<input
+				type="checkbox"
+				class="toggle border-none bg-white [--tglbg:#a729f5] hover:bg-white
+ "
+				checked="checked"
+			/>
+
+			<p>Yearly Billing</p>
+		</div>
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,19 @@
 	<p>Sign-up for our 30-day trial. No credit card required.</p>
 </div>
 
-<div class="h-[300px] w-[500px] content-center rounded-md bg-white">
+<div class="h-[300px] w-[500px] rounded-md bg-white">
 	<p>100K Pageviews</p>
-	<div class="w-[150px]">
+	<div class="w-[400px] place-self-center">
 		<input type="range" min="0" max="100" value="40" class="range range-xs" />
 	</div>
+	<div class="place-content-end text-xs">
+		<ul class="list-disc">
+			<li>Unlimited websites</li>
+			<li>100% data ownership</li>
+			<li>Email reports</li>
+		</ul>
+	</div>
+	<button class="h-5 w-20 place-self-end rounded-xl bg-black text-xs text-white"
+		>Start my trial</button
+	>
 </div>
