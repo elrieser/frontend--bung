25daf470de300f9d6b7adf2c34bd0143ac6bfdf6
Elisa Marie <rieser.elisa@johak.at>
2024-11-22 11:16:09 +0100

    background colour

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 	let { children } = $props();
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-evenly bg-blue-100">
+<div
+	class="--tw-bg-opacity: 1; background-color: rgba(250, 251, 255, var(--tw-bg-opacity)); flex
+min-h-screen flex-col items-center justify-evenly bg-blue-50"
+>
 	{@render children()}
 </div>
