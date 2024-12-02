e882ba3082e1172eb84060e582cecbe0f76c1edc
lepfeiffenberger <90902624+lepfeiffenberger@users.noreply.github.com>
2024-12-02 19:41:17 +0100

    finish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,30 +35,8 @@
 	}
 </script>
 
-<!-- <div class="flex flex-col items-center">
-	<h1 class="text-3xl font-semibold">Simple, traffic-based pricing</h1>
-	<p class="text-sx">Sign-up for our 30-day trial. No credit card required.</p>
-</div>
-
-<div class="flex h-[300px] w-[500px] rounded-md bg-white">
-	<div class="place-self-start">
-		<p>100K Pageviews</p>
-		<div class="place-self-end">
-			<p><b>$ {$price} </b>/{time}</p>
-		</div>
-
-		<div class="w-[400px] place-self-center">
-			<input
-				type="range"
-				min="0"
-				max="32"
-				value={sliderValue}
-				class="range range-accent"
-				on:input={onSliderChange}
-			/>
-		</div> -->
 <div class="mb-12 mt-16 flex flex-col items-center space-y-8">
-	<h1 class="mb-2 text-center text-3xl font-semibold">Simple, traffic-based pricing</h1>
+	<h1 class="mb-2 text-center text-3xl font-bold">Simple, traffic-based pricing</h1>
 	<p class="mb-6 text-center text-sm text-gray-500">
 		Sign-up for our 30-day trial. No credit card required.
 	</p>
@@ -66,16 +44,14 @@
 	<div class="flex w-[400px] flex-col items-center space-y-6 rounded-3xl bg-white p-6 shadow-xl">
 		<div class="mb-6 flex w-full items-center justify-between">
 			<div class="text-center">
-				<p class="text-lg font-semibold text-gray-800">100K Pageviews</p>
+				<p class="text-l font-semibold text-gray-500">100K Pageviews</p>
 			</div>
 
-			<!-- Preis - rechts -->
 			<div class="text-center">
-				<p class="text-3xl font-bold text-gray-800"><b>${$price}</b> / {time}</p>
+				<p class="text-3xl text-gray-500"><b class="text-black">${$price}</b> / {time}</p>
 			</div>
 		</div>
 
-		<!-- Slider für Preis -->
 		<div class="mb-4 w-full">
 			<input
 				type="range"
@@ -90,30 +66,31 @@
 		<div class="flex place-content-center">
 			<div class="flex place-self-end">
 				<div class="flex gap-1">
-					<p class="text-xs">Yearly Billing</p>
+					<button
+						class="h-4 w-20 place-self-end rounded-xl bg-orange-500 text-[10px] text-orange-200"
+						>25% Discount</button
+					>
+					<p class="text-[10px] text-gray-500">Yearly Billing</p>
 
-					<input
-						type="checkbox"
-						class="toggle toggle-xs bg-white"
-						checked="checked"
-						on:click={switchTime}
-					/>
+					<input type="checkbox" class="toggle toggle-xs" checked="checked" on:click={switchTime} />
 
-					<p class="text-xs">Monthly Billing</p>
+					<p class="text-[10px] text-gray-500">Monthly Billing</p>
 				</div>
 			</div>
 		</div>
-		<div class="justify-between">
-			<div class="place-self-start">
+		<div class="mb-6 flex w-full items-center justify-between">
+			<div class="text-[10px] text-gray-500">
 				<ul class="list-disc">
 					<li>Unlimited websites</li>
 					<li>100% data ownership</li>
 					<li>Email reports</li>
 				</ul>
 			</div>
-			<button class="h-5 w-20 place-self-end rounded-xl bg-black text-xs text-white"
-				>Start my trial</button
-			>
+			<div>
+				<button class="h-7 w-40 place-self-end rounded-xl bg-black text-xs text-white"
+					>Start my trial</button
+				>
+			</div>
 		</div>
 	</div>
 </div>
